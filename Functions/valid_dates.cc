1c6af1eab28f8eb34e525adf9e83f312e8de4266
JavierBuenoUll <alu0101627922@ull.edu.es>
2023-11-07 21:33:56 +0000

    valid dates 3

--- Functions/valid_dates.cc
+++ Functions/valid_dates.cc
@@ -22,6 +22,8 @@ bool is_valid_date(const int day, const int month, const int year) {
     if (day > daysInMonth[month]) {
     return false;
     }
+  } else if ((year % 4 != 0 && year % 100 == 0) || (year % 400 != 0) && (month == 2 && day >= 29)) {
+    return false;
   } else { 
     return true;
   }
