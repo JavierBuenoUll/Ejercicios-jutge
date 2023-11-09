b5181d529b38685e7cd7ba15427a6c4c93c6c254
JavierBuenoUll <alu0101627922@ull.edu.es>
2023-11-09 21:13:30 +0000

    sort three por fin

---
+++ Functions-2/sort_three.cc
@@ -0,0 +1,55 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2023-2024
+  *
+  * @author Javier Bueno Calzadilla alu0101627922@ull.edu.es
+  * @date 9/11/2023
+  * @brief El programa consiste en intercambiar el valor de dos variables enteras introducidas.
+  * @bug There are no known bugs
+  */
+
+#include <iostream>
+// ordenar los números de menor a mayor) 
+void sort3(int& a, int& b, int& c) {
+  int temp = b;
+  int temporal_c = c;
+  int temporal_a = a;
+  if (a < b && a < c && b < c) {
+    a = a;
+    b = temp;
+    c = temporal_c;
+  } else if (a < b && a < c && c < b) {
+     a = a;
+     b = temporal_c;
+     c = temp;
+  } else if (b < a && b < c && a < c) {
+    a = b;
+    b = temporal_a;
+    c = temporal_c;
+  } else if (b < a && b < c && c < a) {
+    a = b;
+    b = temporal_c;
+    c = temporal_a;
+  } else if (c < a && c < b && a < b) {
+    a = c;
+    b = temporal_a;
+    c = temp;
+  } else if (c < a && c < b && b < a) {
+    a = c;
+    b = temp;
+    c = temporal_a;
+  }
+  std::cout << a << " " << b << " " << c << " " << std::endl;
+}  
+    
+int main (int argc, char* argv[]) {
+  int numero_a;
+  int numero_b;
+  int numero_c;
+  while (std::cin >> numero_a >> numero_b >> numero_c) {
+  sort3(numero_a, numero_b, numero_c);  
+  }
+  return 0;
+}
